35dac2a8aaf255a3656e35d0f663e16eb1d0ff7b
Chellappan <vrchellappan@gmail.com>
2022-10-08 10:23:46 +0530

    Resolve remote derive

--- src/stats/model.rs
+++ src/stats/model.rs
@@ -1,20 +1,17 @@
 use std::str;
 
 use serde::{Deserialize, Serialize};
-use systemstat::{ByteSize, LoadAverage, Memory, PlatformMemory};
-// use tokio_pg_mapper_derive::PostgresMapper;
+use systemstat::{ByteSize, Memory, PlatformMemory};
 
 extern crate chrono;
 
 #[derive(Deserialize, Serialize, Clone, Debug)]
-// #[pg_mapper(table = "stats")]
+
 pub struct Stats {
     pub loadavg: Loadavg,
     pub cpu_usage: String,
+    #[serde(with = "MemoryRef")]
     pub memory_usage: Memory,
-    // PlatformMemory
-    //Memory
-    // pub current_system_time: NaiveDateTime,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -29,3 +26,52 @@ pub struct Loadavg {
     pub five: f32,
     pub fifteen: f32,
 }
+
+// Serde calls this the definition of the remote type. It is just a copy of the
+// remote data structure. The `remote` attribute gives the path to the actual
+// type we intend to derive code for.
+#[derive(Serialize, Deserialize)]
+#[serde(remote = "PlatformMemory")]
+struct PlatformMemoryDef {
+    #[serde(with = "ByteSizeRef")]
+    pub total: ByteSize,
+    #[serde(with = "ByteSizeRef")]
+    pub active: ByteSize,
+    #[serde(with = "ByteSizeRef")]
+    pub inactive: ByteSize,
+    #[serde(with = "ByteSizeRef")]
+    pub wired: ByteSize,
+    #[serde(with = "ByteSizeRef")]
+    pub free: ByteSize,
+    #[serde(with = "ByteSizeRef")]
+    pub purgeable: ByteSize,
+    #[serde(with = "ByteSizeRef")]
+    pub speculative: ByteSize,
+    #[serde(with = "ByteSizeRef")]
+    pub compressor: ByteSize,
+    #[serde(with = "ByteSizeRef")]
+    pub throttled: ByteSize,
+    #[serde(with = "ByteSizeRef")]
+    pub external: ByteSize,
+    #[serde(with = "ByteSizeRef")]
+    pub internal: ByteSize,
+    #[serde(with = "ByteSizeRef")]
+    pub uncompressed_in_compressor: ByteSize,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(remote = "ByteSize")]
+struct ByteSizeRef(u64);
+
+#[derive(Serialize, Deserialize)]
+#[serde(remote = "Memory")]
+pub struct MemoryRef {
+    #[serde(with = "ByteSizeRef")]
+    pub total: ByteSize,
+    #[serde(with = "ByteSizeRef")]
+    pub free: ByteSize,
+    #[serde(with = "PlatformMemoryDef")]
+    pub platform_memory: PlatformMemory,
+}
+
+// Ref::https://serde.rs/remote-derive.html
